930c542ddc61f3f1026a33e5c83c8722150bffcf
TomasPonce <tomasponce7mo@gmail.com>
2023-08-30 09:18:55 -0300

    0.0.1

---
+++ src/Page/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,10 @@
+import { Logo } from '../../Layout/Logo/Logo'
+import './LoadingScreen.css'
+
+export const LoadingScreen = () => {
+  return (
+    <section className='logosection'>
+      <Logo />
+    </section>
+  )
+}
