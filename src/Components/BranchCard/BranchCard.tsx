bd92e2e46a692ab7a91da75130dd7ca5c0b89ac3
Gonzalez Joaquin <128097621+Gonzalez-Joaquin@users.noreply.github.com>
2023-09-05 11:49:30 -0300

    V - 0.0.9.1 ' Card '

--- src/Components/BranchCard/BranchCard.tsx
+++ src/Components/BranchCard/BranchCard.tsx
@@ -1,75 +1,137 @@
-import { useState } from 'react'
-import { Arrow } from '../SVG/Arrow'
-import { CalendarIcon } from '../SVG/CalendarIcon'
-import { LocationIcon } from '../SVG/LocationIcon'
-import { MailIcon } from '../SVG/MailIcon'
-import { PhoneIcon } from '../SVG/PhoneIcon'
+import { Text } from "../Typography/Typography"
+import { Icon } from "../Icon/Icon"
+
+import { Branch } from "../../Interfaces/BranchInterface"
+import { useState } from "react"
+
 import './BranchCard.css'
+import { useNavigate } from "react-router-dom"
 
 interface Props {
-    branch: {
-        company: string,
-        branch: string,
-        imagesrc: string,
-        address: string,
-        addressmore: string,
-        phonenumber: string,
-        schedule: string,
-        email: string
-    }
+  branch: Branch
 }
 
-export const BranchCard = ( props: Props ) => {
+export const BranchCard = ({ branch }: Props) => {
 
-  const [Expanded, setExpanded] = useState({
-    state: false,
-    value: props.branch.address + "."
-  })
-
-  const ExpandToggle = () => {
-    if(Expanded.state){
-      document.getElementById("collapsible")?.click()
-      document.getElementById("arrow")?.classList.remove("arrowanimation")
-      setExpanded({
-        state: false,
-        value: props.branch.address + "."
-      })
-    
-    }
-    else {
-      document.getElementById("collapsible")?.click()
-      document.getElementById("arrow")?.classList.add("arrowanimation")
-        setExpanded({
-          state: true,
-        value: props.branch.address + ", " + props.branch.addressmore
-        })
-    
-    }
-  }
+  const navigate = useNavigate()
+  const [branchMode, setBranchMode] = useState(Boolean)
 
   return (
-    <div className='branchcard' onClick={ExpandToggle}>
-      <input id="collapsible" className="toggle" type='checkbox' />
-      <div className='togglelabel'>
-      <img className="companyimg" src={props.branch.imagesrc} />
-      <div className='companydata'>
-        <p className='company'>{props.branch.company}</p>
-        <p className='branch'>{props.branch.branch}</p> 
-        <div className='address'>
-        <LocationIcon /><span className='details'>{Expanded.value}</span>
+    <div className={`branch-card ${branchMode ? 'open' : ''}`}>
+      <div className="card-toggle flex">
+        <div className="card-toggle-img">
+          <img src={branch.imagesrc} className="card-img" />
         </div>
-        </div>
-        </div>
-        
-        <div className="collapsiblecontent">
-          <p className='details'><PhoneIcon />{props.branch.phonenumber}</p>
-          <div className='detailsdiv'>
-          <CalendarIcon /><span className='details'>{props.branch.schedule}</span>
+        <div className="card-toggle-data flex">
+          <div className="card-toggle-data-top flex">
+            <div className="card-toggle-data-text">
+              <Text type="h3" style_type="text-title" content={branch.company} size="text-small" styles_color="text-gris-oscuro" />
+              <Text type="p" style_type="text-p" content={branch.branch} styles_color="text-gris" size="text-extra-small" />
+            </div>
+            <div className="card-toggle-data-icon" onClick={() => navigate('/Welcome')}>
+              <Icon icon="sign-out-alt" style_color="icon-violeta" />
+            </div>
           </div>
-          <p className='details'><MailIcon />{props.branch.email}</p>
+          <div className="card-toggle-data-bottom flex">
+            <div className="card-toggle-location flex">
+              <Icon icon="marker" />
+              <Text type="h4" style_type="text-subtitle" content={branch.address} size="text-extra-small" styles_color="text-gris-oscuro" />
+            </div>
+            <button type="button" className="toggle-button flex" onClick={() => setBranchMode(!branchMode)} >
+              <Icon icon={`angle-small-down ${branchMode ? 'close' : ''}`} style_color="icon-gris-clarito" />
+            </button>
+          </div>
         </div>
-
-        <Arrow />
+      </div>
+      <div className={`card-toggle-active flex ${branchMode ? 'open' : ''}`}>
+        <div className="card-active-text flex">
+          <Icon icon='phone-flip' style_color="icon-gris-clarito" />
+          <Text type="p" style_type="text-p" content={branch.phonenumber} styles_color="text-gris-oscuro" size="text-extra-small" />
+        </div>
+        <div className="card-active-text flex">
+          <Icon icon='calendar-day' style_color="icon-gris-clarito" />
+          <Text type="p" style_type="text-p" content={branch.schedule} styles_color="text-gris-oscuro" size="text-extra-small" />
+        </div>
+        <div className="card-active-text flex">
+          <Icon icon='envelope' style_color="icon-gris-clarito" />
+          <Text type="p" style_type="text-p" content={branch.email} styles_color="text-gris-oscuro" size="text-extra-small" />
+        </div>
+      </div>
     </div>
   )
 }
+
+
+// import { useState } from 'react'
+// import { Arrow } from '../SVG/Arrow'
+// import { CalendarIcon } from '../SVG/CalendarIcon'
+// import { LocationIcon } from '../SVG/LocationIcon'
+// import { MailIcon } from '../SVG/MailIcon'
+// import { PhoneIcon } from '../SVG/PhoneIcon'
+// import './BranchCard.css'
+
+// interface Props {
+//   branch: {
+//     company: string,
+//     branch: string,
+//     imagesrc: string,
+//     address: string,
+//     addressmore: string,
+//     phonenumber: string,
+//     schedule: string,
+//     email: string
+//   }
+// }
+
+// export const BranchCard = (props: Props) => {
+
+//   const [Expanded, setExpanded] = useState({
+//     state: false,
+//     value: props.branch.address + "."
+//   })
+
+//   const ExpandToggle = () => {
+//     if (Expanded.state) {
+//       document.getElementById("collapsible")?.click()
+//       document.getElementById("arrow")?.classList.remove("arrowanimation")
+//       setExpanded({
+//         state: false,
+//         value: props.branch.address + "."
+//       })
+
+//     }
+//     else {
+//       document.getElementById("collapsible")?.click()
+//       document.getElementById("arrow")?.classList.add("arrowanimation")
+//       setExpanded({
+//         state: true,
+//         value: props.branch.address + ", " + props.branch.addressmore
+//       })
+
+//     }
+//   }
+
+//   return (
+//     <div className='branchcard' onClick={ExpandToggle}>
+//       <input id="collapsible" className="toggle" type='checkbox' />
+//       <div className='togglelabel'>
+//         <img className="companyimg" src={props.branch.imagesrc} />
+//         <div className='companydata'>
+//           <p className='company'>{props.branch.company}</p>
+//           <p className='branch'>{props.branch.branch}</p>
+//           <div className='address'>
+//             <LocationIcon /><span className='details'>{Expanded.value}</span>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="collapsiblecontent">
+//         <p className='details'><PhoneIcon />{props.branch.phonenumber}</p>
+//         <div className='detailsdiv'>
+//           <CalendarIcon /><span className='details'>{props.branch.schedule}</span>
+//         </div>
+//         <p className='details'><MailIcon />{props.branch.email}</p>
+//       </div>
+//       <Arrow />
+//     </div>
+//   )
+// }
--- src/Layouts/Companys Layout/CompanyCard.tsx
+++ src/Layouts/Companys Layout/CompanyCard.tsx
@@ -1,10 +1,23 @@
-import { Text } from "../../Components/Typography/Typography"
+import { BranchCard } from "../../Components/BranchCard/BranchCard"
+import { Branch } from "../../Interfaces/BranchInterface"
 
 export const CompanyCard = () => {
+
+    const branch: Branch = {
+        company: "Giannoni",
+        branch: "Sucursal 2 - Longchamps",
+        imagesrc: "./Png/giannoniimg.png",
+        address: "Av. Hipólito Yrigoyen 18329",
+        addressmore: "Longchamps (1854) Buenos Aires.",
+        phonenumber: "11 3039 9525",
+        schedule: "Lunes a Viernes 8:00 a 12:30 - 14:00 a 18:30 Sábado 8:00 a 13:00",
+        email: "longchampas02@pintureriagiannoni.com"
+    }
+
     return (
         <>
             <div className="companysArticle-content card flex">
-                <Text type={"h1"} style_type={"text-title"} content={"Lo De Tomi"} />
+                <BranchCard branch={branch} />
             </div>
         </>
     )
