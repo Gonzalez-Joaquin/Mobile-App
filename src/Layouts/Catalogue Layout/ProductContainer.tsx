c88aa4133d7eccfde4f24eb9b2c445dadb3f6fdd
Gonzalez Joaquin <128097621+Gonzalez-Joaquin@users.noreply.github.com>
2023-09-06 09:53:42 -0300

    V - 0.0.9

--- src/Layouts/Catalogue Layout/ProductContainer.tsx
+++ src/Layouts/Catalogue Layout/ProductContainer.tsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../../App/store"
+
 export const ProductContainer = () => {
+
+    const items = useSelector((store: RootState) => store.items.items)
+
     return (
-        <>
-            hola
-        </>
+        <div className="product-container flex">
+            {
+                //aca irian con el map!
+                // items.map(item => {
+                // <Item item={item} />
+                // })
+            }
+        </div>
     )
 }
