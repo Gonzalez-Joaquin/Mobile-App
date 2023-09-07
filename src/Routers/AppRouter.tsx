7b0a4120f8e16b92dcaa3cc778437bb3e17ada2a
TomasPonce <tomasponce7mo@gmail.com>
2023-09-07 11:09:16 -0300

    0.0.6

--- src/Routers/AppRouter.tsx
+++ src/Routers/AppRouter.tsx
@@ -5,12 +5,12 @@ import { Companys } from "../Pages/Companys/Companys"
 import { Welcome } from "../Pages/Welcome/Welcome"
 import { Product } from "../Pages/Product/Product"
 import { Catalogue } from "../Pages/Catalogue/Catalogue"
-import { ListItem } from "../Components/Item/ListItem"
+
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<ListItem />} />
+      <Route path="/" element={<StartScreen />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Welcome" element={<Welcome />} />
       <Route path="/Company" element={<Companys />} />
